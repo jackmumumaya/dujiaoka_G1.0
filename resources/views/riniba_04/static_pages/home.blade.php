c7afa93ac04222e4e8656d678df6d1b75fe8bd25
jackmumumaya <jackmumumaya@users.noreply.github.com>
2026-02-16 10:20:00 +0800

    add product filter buttons to riniba_04 and luna themes

--- resources/views/riniba_04/static_pages/home.blade.php
+++ resources/views/riniba_04/static_pages/home.blade.php
@@ -14,46 +14,65 @@
         </div>
     </div>
 
+    <div class="row mb-3" style="margin-top: 15px;">
+        <div class="col-12 text-center">
+            <div class="btn-group shadow-sm">
+                <a href="{{ request()->fullUrlWithQuery(['sort' => null]) }}"
+                    class="btn btn-sm {{ !request('sort') ? 'btn-dark' : 'btn-outline-dark' }}">
+                    {{ __('dujiaoka.default_sort') }}
+                </a>
+                <a href="{{ request()->fullUrlWithQuery(['sort' => 'sales_volume']) }}"
+                    class="btn btn-sm {{ request('sort') == 'sales_volume' ? 'btn-info' : 'btn-outline-info' }}">
+                    {{ __('dujiaoka.sales_volume') }}
+                </a>
+                <a href="{{ request()->fullUrlWithQuery(['filter' => request('filter') == 'in_stock' ? null : 'in_stock']) }}"
+                    class="btn btn-sm {{ request('filter') == 'in_stock' ? 'btn-success' : 'btn-outline-success' }}">
+                    {{ __('dujiaoka.in_stock_only') }}
+                </a>
+            </div>
+        </div>
+    </div>
+
     <div class="nav home-nav-list">
         <a href="#group-all" class="home-tab-link active" data-bs-toggle="tab" aria-expanded="false" role="tab"
             data-toggle="tab">
             <span class="tab-title">{{ __('dujiaoka.group_all') }}</span>
         </a>
         @foreach($data as $index => $group)
-        <a href="#group-{{ $group['id'] }}" class="home-tab-link" data-bs-toggle="tab" aria-expanded="false" role="tab"
-            data-toggle="tab">
-            <span class="tab-title">{{
-                $group['gp_name'] }}</span>
-        </a>
-    
+            <a href="#group-{{ $group['id'] }}" class="home-tab-link" data-bs-toggle="tab" aria-expanded="false" role="tab"
+                data-toggle="tab">
+                <span class="tab-title">{{
+            $group['gp_name'] }}</span>
+            </a>
+
         @endforeach
-    
+
     </div>
     <div class="home-main-content">
 
-        
 
 
 
-<div class="tab-pane active" id="group-all">
-    <div class="home-wrapper">
+
+        <div class="tab-pane active" id="group-all">
+            <div class="home-wrapper">
+                @foreach($data as $group)
+                    @foreach($group['goods'] as $goods)
+                        @include('riniba_04.layouts._goods', ['goods' => $goods])
+                    @endforeach
+                @endforeach
+            </div>
+        </div>
+
         @foreach($data as $group)
-        @foreach($group['goods'] as $goods)
-        @include('riniba_04.layouts._goods', ['goods' => $goods])
+            <div class="tab-pane" id="group-{{ $group['id'] }}">
+                <div class="home-wrapper">
+                    @foreach($group['goods'] as $goods)
+                        @include('riniba_04.layouts._goods', ['goods' => $goods])
+                    @endforeach
+                </div>
+            </div>
         @endforeach
-        @endforeach
-    </div>
-</div>
-
-@foreach($data as $group)
-<div class="tab-pane" id="group-{{ $group['id'] }}">
-    <div class="home-wrapper">
-        @foreach($group['goods'] as $goods)
-        @include('riniba_04.layouts._goods', ['goods' => $goods])
-        @endforeach
-    </div>
-</div>
-@endforeach
 
     </div>
 </div>
@@ -70,9 +89,9 @@
             $(".category").show();
         }
     });
-        function sell_out_tip() {
-            $.NotificationApp.send("提示", "商品缺货", "top-center", "rgba(0,0,0,0.2)", "info");
-        }
-    </script>
-    
+    function sell_out_tip() {
+        $.NotificationApp.send("提示", "商品缺货", "top-center", "rgba(0,0,0,0.2)", "info");
+    }
+</script>
+
 @stop
